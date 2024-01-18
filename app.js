6ab4f311cbd4c04a1895ea0bf6c5e9d84a487e01
narajaon <rajaonarisonfabien@yahoo.fr>
2024-01-18 14:51:24 +0900

    feat: setup express / htmx / tailwindCSS

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	console.log("/GET /");
+	res.render(__dirname + "/views/index", { body: "EJS" });
+});
+
+app.get("/test", (req, res) => {
+	console.log("/GET /test");
+	res.render(__dirname + "/views/body", { body: "Clicked" });
+});
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
