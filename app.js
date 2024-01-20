0ba32de51ae5fbe92efa483a43440f4981f8328a
narajaon <rajaonarisonfabien@yahoo.fr>
2024-01-20 20:54:23 +0900

    feat: dynamic page title

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (_, res) => {
-	res.render(__dirname + "/views/pages/home");
+	res.render(__dirname + "/views/pages/home", {
+		head: { title: "home page" },
+	});
 });
 
 app.get("/schedule", (req, res) => {
 	res.render(__dirname + "/views/pages/schedule", {
 		months,
 		currentDay: req.query.currentDay,
+		head: { title: "schedule page" },
 	});
 });
 
